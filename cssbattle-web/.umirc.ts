a4ad5954cafc7fadb4613d729e9471f7e077d514
wuwz <wuwz@yxt.com>
2021-10-21 11:54:35 +0800

    chore(all): release v0.0.36

--- cssbattle-web/.umirc.ts
+++ cssbattle-web/.umirc.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     {
       src:
         process.env.NODE_ENV === 'production'
-          ? 'https://cdn.wuwenzhou.com.cn/web/0.0.35/lsp.js'
+          ? 'https://cdn.wuwenzhou.com.cn/web/0.0.36/lsp.js'
           : '/lsp.js',
     },
   ],
@@ -31,7 +31,7 @@ export default defineConfig({
       rel: 'icon',
       href:
         process.env.NODE_ENV === 'production'
-          ? 'https://cdn.wuwenzhou.com.cn/web/0.0.35/favicon.ico'
+          ? 'https://cdn.wuwenzhou.com.cn/web/0.0.36/favicon.ico'
           : '/favicon.ico',
     },
   ],
@@ -42,7 +42,7 @@ export default defineConfig({
   fastRefresh: {},
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? 'https://cdn.wuwenzhou.com.cn/web/0.0.35/'
+      ? 'https://cdn.wuwenzhou.com.cn/web/0.0.36/'
       : '/',
   proxy: {
     '/api': {
@@ -53,8 +53,8 @@ export default defineConfig({
   },
   // devServer: {
   //   https: {
-  //     key: './Nginx/2_cssbattle.wuwenzhou.com.cn/web/0.0.35/',
-  //     cert: './Nginx/1_cssbattle.wuwenzhou.com.cn/web/0.0.35/',
+  //     key: './Nginx/2_cssbattle.wuwenzhou.com.cn/web/0.0.36/',
+  //     cert: './Nginx/1_cssbattle.wuwenzhou.com.cn/web/0.0.36/',
   //   },
   // },
 });
